edc85917aec04c5c24f8ac0755b12a4d8fa46ce3
Robert Ludwick <>
2019-02-16 09:25:23 -0500

    changed the template flask is using

--- bee-careful.py
+++ bee-careful.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-	return render_template('homescreen.html')
+	return render_template('templates2.html')
 	
 @app.route("/news")
 def news():
