7b3dc97531d61d00f720fb3d24eca158546c8cf2
Robert Ludwick <>
2019-02-16 03:01:26 -0500

    Created the individual webpages in flask adding placeholder outputs

--- bee-careful.py
+++ bee-careful.py
@@ -4,3 +4,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
 	return "Hello Kori!"
+	
+@app.route("/news")
+def news():
+	return "news"
+	
+@app.route("/Submit")
+def submission():
+	return "submit"
+	
+@app.route("/contactUs")
+def contact():
+	return "Karan Erry"
